ddded1444510c7b0ae79b8ab3d8cbb4647c87a00
Kiran S <6carbon12.oss@proton.me>
2025-08-12 21:23:05 +0530

    feat(client): Initialize React with Vite

---
+++ client/vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  server: {
+    port: 3000,
+    strictPort: true,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:4000',
+      }
+    }
+  }
+})
